5e8ec20a281dfd1a44cfad83b4781fc4c6abdf79
Kudzeri <kudzeri@gmail.com>
2024-12-10 09:57:23 +0500

    ...

--- src/axios.js
+++ src/axios.js
@@ -5,8 +5,11 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use((config) => {
-  config.headers.Authorization = localStorage.getItem("token");
-
+  if (typeof localStorage !== "undefined" && localStorage.getItem("token")) {
+    config.headers.Authorization = localStorage.getItem("token");
+  } else {
+    console.error("localStorage is not available or token is missing.");
+  }
   return config;
 });
 
